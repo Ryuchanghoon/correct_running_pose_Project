6df7fa2a273df2222a684c814b3fe314b32a927d
fbckdgns3@naver.com <fbckdgns3@naver.com>
2024-05-31 19:25:53 +0900

    feat: 팔, 무릎, 목 각도 추출 초기 세팅

---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import cv2
+import mediapipe as mp
+import numpy as np
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  
+    b = np.array(b)  
+    c = np.array(c) 
+    
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+    
+    if angle > 180.0:
+        angle = 360.0 - angle
+        
+    return angle
+
+
+st.title("런닝 바른 자세")
+st.write("본인 영상 올리세용~")
+uploaded_file = st.file_uploader("Choose your video", type=["mp4", "mov", "avi"])
+
+if uploaded_file is not None:
+    cap = cv2.VideoCapture(uploaded_file.name)
+    
+    with mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            
+            results = pose.process(image)
+            
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            
+            if results.pose_landmarks:
+                landmarks = results.pose_landmarks.landmark
+                
+                left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x, landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+                left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+                
+                
+                left_elbow_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
+                left_knee_angle = calculate_angle(left_hip, left_knee, left_ankle)
+                neck_angle = calculate_angle([landmarks[mp_pose.PoseLandmark.NOSE.value].x, landmarks[mp_pose.PoseLandmark.NOSE.value].y],
+                                             [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y],
+                                             [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y])
+                
+                
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+                cv2.putText(image, f'Elbow Angle: {int(left_elbow_angle)}', 
+                            tuple(np.multiply(left_elbow, [640, 480]).astype(int)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                cv2.putText(image, f'Knee Angle: {int(left_knee_angle)}', 
+                            tuple(np.multiply(left_knee, [640, 480]).astype(int)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                cv2.putText(image, f'Neck Angle: {int(neck_angle)}', 
+                            (50, 50), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+            
+            st.image(image, channels="BGR")
+            
+    cap.release()
